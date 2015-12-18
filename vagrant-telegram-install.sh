6fc907ee20f7e601702dd43a9ddc94d4e2663954
Camilo Sperberg <me@unreal4u.com>
2015-12-18 01:23:38 +0100

    Say always yes

--- vagrant-telegram-install.sh
+++ vagrant-telegram-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make
+sudo apt-get -y update
+sudo apt-get -y install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make
 cd ~
 git clone --recursive https://github.com/vysheng/tg.git
 cd tg
