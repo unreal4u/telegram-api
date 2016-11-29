18946db70e9a20397b67d7d0bc8b1f12e1c342c3
Camilo Sperberg <me@unreal4u.com>
2016-11-29 01:30:33 +0100

    Improve tests

--- tests/Telegram/Types/InlineKeyboardMarkupTest.php
+++ tests/Telegram/Types/InlineKeyboardMarkupTest.php
@@ -5,7 +5,9 @@ namespace unreal4u\TelegramAPI\tests\Telegram\Types;
 use PHPUnit_Framework_TestCase as TestCase;
 #use PHPUnit\Framework\TestCase;
 use unreal4u\TelegramAPI\Telegram\Types\CallbackQuery;
+use unreal4u\TelegramAPI\Telegram\Types\Chat;
 use unreal4u\TelegramAPI\Telegram\Types\Update;
+use unreal4u\TelegramAPI\Telegram\Types\User;
 use unreal4u\TelegramAPI\tests\Mock\MockTgLog;
 
 class InlineKeyboardMarkupTest extends TestCase
@@ -36,9 +38,14 @@ class InlineKeyboardMarkupTest extends TestCase
     public function testIncomingUpdate()
     {
         $postData = $this->tgLog->getTestTypeResponse('InlineKeyboardMarkup');
+        /** @var Update $update */
         $update = new Update($postData);
 
         $this->assertInstanceOf(CallbackQuery::class, $update->callback_query);
-        #var_dump($update);
+        $this->assertInstanceOf(User::class, $update->callback_query->message->from);
+        $this->assertInstanceOf(Chat::class, $update->callback_query->message->chat);
+        $this->assertCount(1, $update->callback_query->message->entities);
+        $this->assertSame(56, $update->callback_query->message->reply_to_message->message_id);
+        $this->assertSame('-1234567890123456789', $update->callback_query->chat_instance);
     }
 }
