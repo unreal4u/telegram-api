c1a72ef76b0c06d3c1fb04738f3266519f7e081a
Camilo Sperberg <me@unreal4u.com>
2017-06-25 18:43:21 +0200

    Fixes #35

--- examples/webhookinfo.php
+++ examples/webhookinfo.php
@@ -4,12 +4,12 @@ declare(strict_types = 1);
 
 include 'basics.php';
 
-use unreal4u\TelegramAPI\Telegram\Methods\getWebhookInfo;
+use unreal4u\TelegramAPI\Telegram\Methods\GetWebhookInfo;
 use unreal4u\TelegramAPI\TgLog;
 
 $tgLog = new TgLog(BOT_TOKEN);
 
-$webHookInfo = new getWebhookInfo();
+$webHookInfo = new GetWebhookInfo();
 
 $hookInfo = $tgLog->performApiRequest($webHookInfo);
 echo '<pre>';
