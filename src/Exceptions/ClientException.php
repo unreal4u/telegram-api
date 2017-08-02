218899a26dc0c2071b3dc65b65bce057173bd121
Camilo Sperberg <me@unreal4u.com>
2017-08-02 23:15:04 +0200

    Don't define values that are already defined as such

--- src/Exceptions/ClientException.php
+++ src/Exceptions/ClientException.php
@@ -12,7 +12,7 @@ class ClientException extends \RuntimeException
     /**
      * @var UnsuccessfulRequest
      */
-    protected $errorRequest = null;
+    protected $errorRequest;
 
     public function __construct(string $message = '', int $code = 0, Throwable $previous = null)
     {
