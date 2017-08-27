82478e6232ba99b6414331f83fe199bcc4a29e7d
Yoshi2889 <rick.2889@gmail.com>
2017-08-27 22:53:52 +0200

    Fix send-chat-action example
    sleep()-ing has no effect if you do it before the event loop starts
    
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- examples/send-chat-action.php
+++ examples/send-chat-action.php
@@ -21,25 +21,27 @@ $sendChatAction = new SendChatAction();
 $sendChatAction->chat_id = A_USER_CHAT_ID;
 $sendChatAction->action = 'typing';
 
-$tgLog->performApiRequest($sendChatAction);
-// Some heavy operation you have to perform, in our case, it will 2 seconds
-sleep(2);
+$promise = $tgLog->performApiRequest($sendChatAction);
 
-$sendMessage->text = 'The second piece of text';
+$promise->then(function () use ($tgLog, $sendMessage) {
+    sleep(3);
 
-$promise = $tgLog->performApiRequest($sendMessage);
+    $sendMessage->text = 'The second piece of text';
 
-$promise->then(
-    function ($response) {
-        echo '2nd message sent' . PHP_EOL;
-        echo '<pre>';
-        var_dump($response);
-        echo '</pre>';
-    },
-    function (\Exception $exception) {
-        // Onoes, an exception occurred...
-        echo 'Exception ' . get_class($exception) . ' caught, message: ' . $exception->getMessage();
-    }
-);
+    $promise = $tgLog->performApiRequest($sendMessage);
+
+    $promise->then(
+        function ($response) {
+            echo '2nd message sent' . PHP_EOL;
+            echo '<pre>';
+            var_dump($response);
+            echo '</pre>';
+        },
+        function (\Exception $exception) {
+            // Onoes, an exception occurred...
+            echo 'Exception ' . get_class($exception) . ' caught, message: ' . $exception->getMessage();
+        }
+    );
+});
 
 $loop->run();
