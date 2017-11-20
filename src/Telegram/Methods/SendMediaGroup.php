793a10b9adbe3c78d87625708f74e6bf3145e808
Camilo Sperberg <me@unreal4u.com>
2017-11-20 23:05:31 +0100

    First part of the new API: Allow an album to be sent

---
+++ src/Telegram/Methods/SendMediaGroup.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace unreal4u\TelegramAPI\Telegram\Methods;
+
+use Psr\Log\LoggerInterface;
+use unreal4u\TelegramAPI\Abstracts\TelegramMethods;
+use unreal4u\TelegramAPI\Abstracts\TelegramTypes;
+use unreal4u\TelegramAPI\InternalFunctionality\TelegramRawData;
+use unreal4u\TelegramAPI\Telegram\Types\Custom\MessageArray;
+use unreal4u\TelegramAPI\Telegram\Types\InputMedia;
+
+/**
+ * Use this method to send photos. On success, the sent Message is returned
+ *
+ * Objects defined as-is January 2017
+ *
+ * @see https://core.telegram.org/bots/api#sendphoto
+ */
+class SendMediaGroup extends TelegramMethods
+{
+    /**
+     * Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+     * @var string
+     */
+    public $chat_id = '';
+
+    /**
+     * A JSON-serialized array describing photos and videos to be sent
+     * @var InputMedia[]
+     */
+    public $media = [];
+
+    /**
+     * Optional. Sends the message silently. iOS users will not receive a notification, Android users will receive a
+     * notification with no sound.
+     * @see https://telegram.org/blog/channels-2-0#silent-messages
+     * @var bool
+     */
+    public $disable_notification = false;
+
+    /**
+     * Optional. If the message is a reply, ID of the original message
+     * @var int
+     */
+    public $reply_to_message_id = 0;
+
+    public function getMandatoryFields(): array
+    {
+        return [
+            'chat_id',
+            'media',
+        ];
+    }
+    
+    public static function bindToObject(TelegramRawData $data, LoggerInterface $logger): TelegramTypes
+    {
+        return new MessageArray($data, $logger);
+    }
+}
---
+++ src/Telegram/Types/Custom/MessageArray.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types = 1);
+
+namespace unreal4u\TelegramAPI\Telegram\Types\Custom;
+
+use unreal4u\TelegramAPI\Abstracts\CustomType;
+use Psr\Log\LoggerInterface;
+use unreal4u\TelegramAPI\Interfaces\CustomArrayType;
+use unreal4u\TelegramAPI\Telegram\Types\Message;
+
+/**
+ * Used for methods that will return an array of messages
+ */
+class MessageArray extends CustomType implements CustomArrayType
+{
+    public function __construct(array $data = null, LoggerInterface $logger = null)
+    {
+        if (count($data) !== 0) {
+            foreach ($data as $telegramResponse) {
+                // Create an actual Update object and fill the array
+                $this->data[] = new Message($telegramResponse, $logger);
+            }
+        }
+    }
+
+    /**
+     * Traverses through our $data, yielding the result set
+     *
+     * @return Update[]
+     */
+    public function traverseObject()
+    {
+        foreach ($this->data as $message) {
+            yield $message;
+        }
+    }
+}
