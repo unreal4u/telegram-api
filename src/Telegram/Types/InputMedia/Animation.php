2feeaac4a863b2022666f634f236f754eda8bbfe
Camilo Sperberg <me@unreal4u.com>
2018-07-26 23:05:57 +0200

    PSR-2 fixes

--- src/Telegram/Types/InputMedia/Animation.php
+++ src/Telegram/Types/InputMedia/Animation.php
@@ -24,8 +24,8 @@ class Animation extends InputMedia
 
     /**
      * Optional. Thumbnail of the file sent. The thumbnail should be in JPEG format and less than 200 kB in size. A
-     * thumbnail‘s width and height should not exceed 90. Ignored if the file is not uploaded using multipart/form-data.
-     * Thumbnails can’t be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>”
+     * thumbnail's width and height should not exceed 90. Ignored if the file is not uploaded using multipart/form-data.
+     * Thumbnails can't be reused and can be only uploaded as a new file, so you can pass "attach://<file_attach_name>"
      * if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
      * @var InputFile
      */
