bcb1530e5a2bcb471d2db2a144607a058b1d16d8
Camilo Sperberg <me@unreal4u.com>
2018-08-30 23:45:50 +0200

    Fixing last BC issues from v2, also updating dependencies

--- src/Telegram/Types/Custom/PassportFileArray.php
+++ src/Telegram/Types/Custom/PassportFileArray.php
@@ -4,16 +4,15 @@ declare(strict_types = 1);
 
 namespace unreal4u\TelegramAPI\Telegram\Types\Custom;
 
-use unreal4u\TelegramAPI\Abstracts\CustomType;
+use unreal4u\TelegramAPI\Abstracts\TraversableCustomType;
 use unreal4u\TelegramAPI\Telegram\Types\Passport\PassportFile;
 use unreal4u\TelegramAPI\Telegram\Types\PhotoSize;
-use unreal4u\TelegramAPI\Interfaces\CustomArrayType;
 use Psr\Log\LoggerInterface;
 
 /**
  * Mockup class to generate a real telegram update representation
  */
-class PassportFileArray extends CustomType implements CustomArrayType
+class PassportFileArray extends TraversableCustomType
 {
     public function __construct(array $data = null, LoggerInterface $logger = null)
     {
