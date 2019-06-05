bb375cbfde220e811720b1554e522512774d1859
camilo.sperberg <camilo.sperberg@isaac.nl>
2019-06-06 00:40:06 +0200

    General maintenance
    - Removed vagrant, added Docksal instead
    - Added PHP 7.3 to the list of testing in travis
    - Updated the composer.lock file to include dependencies for max. PHP 7.0

--- bin/composer-update.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-vagrant ssh -- -t 'cd telegram-api/; /usr/bin/composer.phar update -o --prefer-dist'
--- bin/run-tests.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf tests/report/
-vagrant ssh -- -t 'cd telegram-api/; /usr/bin/phpdbg -qrr vendor/bin/phpunit'
