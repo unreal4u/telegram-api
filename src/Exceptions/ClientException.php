faeb241886f723a11575b8570620643e08a3729e
NanoSector <rick@nanosector.nl>
2021-08-09 19:24:27 +0200

    Decode Telegram response from ReactPHP's ResponseException
    Signed-off-by: Rick Kerkhof <rick@nanosector.nl>

--- src/Exceptions/ClientException.php
+++ src/Exceptions/ClientException.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace unreal4u\TelegramAPI\Exceptions;
 
+use React\Http\Message\ResponseException;
 use Throwable;
 use unreal4u\TelegramAPI\Telegram\Types\Custom\UnsuccessfulRequest;
 
@@ -27,6 +28,34 @@ class ClientException extends \RuntimeException
                 'error_code' => $previous->getCode(),
             ]));
         }
+    }
+
+    /**
+     * Construct a new ClientException with the parameters and, if present, body
+     * found in the given ResponseException.
+     *
+     * @param  \React\Http\Message\ResponseException  $responseException
+     *
+     * @return static
+     */
+    public static function fromResponseException(ResponseException $responseException): self {
+        $exception = new ClientException(
+            $responseException->getMessage(),
+            $responseException->getCode(),
+            $responseException
+        );
+
+        $body    = $responseException->getResponse()->getBody()->getContents();
+        $decoded = json_decode($body, true);
+
+        if (is_array($decoded)
+            && array_key_exists('description', $decoded)
+            && array_key_exists('error_code', $decoded)
+        ) {
+            $exception->setError(new UnsuccessfulRequest($decoded));
+        }
+
+        return $exception;
     }
 
     public function setError(UnsuccessfulRequest $unsuccessfulRequest): ClientException
