fa0e2bb026f7d545535478ef824e9bc3ca0b6a06
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-09-14 21:18:29 +0200

    File upload: minor refactoring, enhanced some examples

--- examples/send-document.php
+++ examples/send-document.php
@@ -6,9 +6,11 @@ include __DIR__.'/basics.php';
 
 use React\EventLoop\Factory;
 use unreal4u\TelegramAPI\HttpClientRequestHandler;
+use unreal4u\TelegramAPI\Telegram\Methods\GetFile;
 use unreal4u\TelegramAPI\Telegram\Methods\SendDocument;
 use unreal4u\TelegramAPI\Telegram\Types\Custom\InputFile;
 use unreal4u\TelegramAPI\TgLog;
+use function Clue\React\Block\await;
 
 $loop = Factory::create();
 $tgLog = new TgLog(BOT_TOKEN, new HttpClientRequestHandler($loop));
@@ -16,14 +18,28 @@ $tgLog = new TgLog(BOT_TOKEN, new HttpClientRequestHandler($loop));
 $sendDocument = new SendDocument();
 $sendDocument->chat_id = A_USER_CHAT_ID;
 $sendDocument->document = new InputFile(__FILE__);
+$sendDocument->thumb = new InputFile(__DIR__ . '/binary-test-data/logo-php7-telegram-bot-api-thumbnail.jpg');
 
 $promise = $tgLog->performApiRequest($sendDocument);
 
 $promise->then(
-    function ($response) {
+    function ($response) use ($tgLog, $loop) {
         echo '<pre>';
         var_dump($response);
         echo '</pre>';
+        /** @var $response \unreal4u\TelegramAPI\Telegram\Types\Message */
+
+        // Load uploaded video and generate URL to download using bot token
+        $getFileVideo = new GetFile();
+        $getFileVideo->file_id = $response->document->file_id;
+        $fileVideo = await($tgLog->performApiRequest($getFileVideo), $loop);
+        var_dump('Document url: ' . $tgLog->fileUrl($fileVideo));
+
+        // Load uploaded thumbnail and generate URL to download using bot token
+        $getFileThumb = new GetFile();
+        $getFileThumb->file_id = $response->document->thumb->file_id;
+        $fileThumb = await($tgLog->performApiRequest($getFileThumb), $loop);
+        var_dump('Thumbnail url: ' . $tgLog->fileUrl($fileThumb));
     },
     function (\Exception $exception) {
         // Onoes, an exception occurred...
