b81a336a0af67cdeb66b2d061b945457467e79f3
LucaDevelop <lsirri.gm@gmail.com>
2022-01-20 22:17:13 +0100

    Added example of entities decodification

---
+++ examples/get-updates-and-decode-entities.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+include __DIR__.'/basics.php';
+
+use React\EventLoop\Factory;
+use \unreal4u\TelegramAPI\HttpClientRequestHandler;
+use unreal4u\TelegramAPI\Telegram\Methods\GetUpdates;
+use \unreal4u\TelegramAPI\Abstracts\TraversableCustomType;
+use unreal4u\TelegramAPI\TgLog;
+
+$loop = Factory::create();
+$tgLog = new TgLog(BOT_TOKEN, new HttpClientRequestHandler($loop));
+
+$getUpdates = new GetUpdates();
+
+// If using this method, send an offset (AKA last known update_id) to avoid getting duplicate update notifications.
+#$getUpdates->offset = 328221148;
+$updatePromise = $tgLog->performApiRequest($getUpdates);
+$updatePromise->then(
+    function (TraversableCustomType $updatesArray) {
+        foreach ($updatesArray as $update) {
+            $decodedText = $update->message->decodeEntities('HTML'); //or MarkdownV2 or Markdown (abandoned, only for backward compatibility)
+            echo $decodedText;
+        }
+    },
+    function (\Exception $exception) {
+        // Onoes, an exception occurred...
+        echo 'Exception ' . get_class($exception) . ' caught, message: ' . $exception->getMessage();
+    }
+);
+
+$loop->run();
